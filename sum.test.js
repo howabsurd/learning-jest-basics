944436f935bd3224dc571998deb36d60dfab0fd0
howabsurd <poptanikrish1@gmail.com>
2024-05-08 17:14:29 +0530

    Some more test cases

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,17 @@
-const sum = require("./sum");
+// const sum = require("./sum");
 
-test("adds 1+ 2", ()=>{
-    expect(sum(1,2)).toBe(3);
+// test("adds 1+ 2", ()=>{
+//     expect(sum(1,2)).toBe(3);
+// })
+
+
+// test("object assignment", ()=>{
+//     const data = {one : 1};
+//     data["two"] = 2;
+//     expect(data).toEqual({one:1 , two : 2});
+// })
+
+test("n is null", ()=>{
+    const n = null;
+    expect(n).toBeFalsy();
 })
